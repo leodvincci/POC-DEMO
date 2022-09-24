6c30a6286b5e8e38755ada89664a25fac64844b8
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 <lpenrose@depaul.edu>
2022-09-23 20:33:13 -0500

    initial commit

---
+++ src/main/java/com/csc363/group2/poc_demo/PocDemoApplication.java
@@ -0,0 +1,13 @@
+package com.csc363.group2.poc_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csc363/group2/poc_demo/PocDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csc363.group2.poc_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
