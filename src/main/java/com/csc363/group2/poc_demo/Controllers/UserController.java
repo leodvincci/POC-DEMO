71d21319c64c40641708f337fc3cd4c72b664c8f
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 <lpenrose@depaul.edu>
2022-09-25 21:09:45 -0500

    bash: line 1: quit: command not found fixed the cors issue, i think

--- src/main/java/com/csc363/group2/poc_demo/Controllers/UserController.java
+++ src/main/java/com/csc363/group2/poc_demo/Controllers/UserController.java
@@ -1,63 +1,44 @@
 package com.csc363.group2.poc_demo.Controllers;
-
 import com.csc363.group2.poc_demo.Services.UserService;
 import com.csc363.group2.poc_demo.appuser.AppUser;
 import com.csc363.group2.poc_demo.appuser.AppUserModelBody;
 import com.csc363.group2.poc_demo.appuser.AppUserService;
 import com.csc363.group2.poc_demo.security.UpdatePasswordBody;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 import java.util.List;
-
 @CrossOrigin(origins = "*")
 @RestController
 public class UserController {
-
-
-
-
     @Autowired
     public UserService userService;
-
     @Autowired
     public AppUserService appUserService;
 
 
-    @CrossOrigin(origins = "*")
+
+
+    @CrossOrigin(origins ="*")
     @GetMapping("/allusers")
     public List<AppUser> getAllUsers(){
         return userService.getListofUsers();
     }
-
-
     @GetMapping("/removeuser")
     @ResponseBody
     public void deleteAllUsers(@RequestParam Long id){
         userService.deleteUser(id);
-
     }
-
     @PostMapping("api/v1/resetpassword")
     @ResponseBody
     public void resetUserPassword(@RequestBody UpdatePasswordBody updatePasswordBody){
         System.out.println("Password has been reset for " + updatePasswordBody.getEmail());
         System.out.println("Password:" + updatePasswordBody.getPassword());
         userService.setNewPassword(updatePasswordBody.getEmail(), appUserService.generateNewPassword(updatePasswordBody.getPassword()));
-
     }
-
-
     @PostMapping("api/v1/updateuserinfo")
     @ResponseBody
     public void updateUserInfo(@RequestBody AppUserModelBody appUserModelBody){
         userService.updateUser(appUserModelBody.getFirstName(),appUserModelBody.getLastName(),appUserModelBody.getEmail(),appUserModelBody.getPassword(),appUserModelBody.getRole());
     }
-
 }
