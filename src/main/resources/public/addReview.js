f43ca4ab6bccb4cddec677efdbbbb32c02db0d16
Esther Mathunny <emathunn@depaul.edu>
2022-10-21 11:20:15 -0500

    adding review + added SavedClasses.html + navbar

--- src/main/resources/public/addReview.js
+++ src/main/resources/public/addReview.js
@@ -1,5 +1,6 @@
 var likes = false;
 var dislikes = false;
+
 function updateLikes(){
     likes = true;
     dislikes = false;
@@ -8,40 +9,61 @@ function updateDislikes(){
     dislikes = true;
     likes = false;
 }
+
+
+//function onClick(){
+//    const email = document.getElementById('email');
+//    const course = document.getElementById('course').split(" ");
+//    let department = course[0];
+//        let number = course[1];
+//    const gpa = document.getElementById('gpa');
+//    const difficultyRating = document.getElementById('difficultyRating');
+//        likeDislikeNone = 0;
+//            if(likes){
+//                likeDislikeNone = 2;
+//            }else if(dislikes){
+//                likeDislikeNone = 1;
+//            }
+//                const classDescription = document.getElementById('classDescription');
+//alert(email +" "+course +" "+department +" "+number +" "+gpa +" "+difficultyRating +" YY"+likeDislikeNone +"YY "+classDescription);
+//
+//}
+
+
 const form = document.getElementById('formElem');
+const course = form.getElementById('course').split(" ");
 form.addEventListener('submit',function (e){
     e.preventDefault();
     const prepayload = new FormData(form)
     const payload = new URLSearchParams(prepayload);
+
     let email = payload.get("email");
-    let course = payload.get("course");
+    let department = course[0];
+    let number = course[1];
     let gpa = payload.get("gpa");
     let difficultyRating = payload.get("difficultyRating");
-    let like = 0;
-    let dislike = 0;
+    likeDislikeNone = 0;
+    alert("hello");
     if(likes){
-        like += 1;
+        like = 2;
     }else if(dislikes){
-        dislike += 1;
+        likeDislikeNone = 1;
     }
     let classDescription = payload.get("classDescription");
-
-alert(email +" "+course +" "+gpa +" "+difficultyRating +" "+like +" "+dislike +" "+classDescription)
+alert(likeDislikeNone);
+alert(email +" "+course +" "+department +" "+number +" "+gpa +" "+difficultyRating +" "+likeDislikeNone +" "+classDescription);
 //call the API
-    // let data = {
-    //     classDescription:classDescription,
-    //     className:className,
-    //     department:department,
-    //     classNumber: classNumber
-    // }
+     let data = {
 
-    // fetch('http://localhost:8080/api/v1/addClass', {
-    //     method: "POST",
-    //     headers: {
-    //         "Content-Type":'application/json'
-    //     },
-    //     body: JSON.stringify(data),
+     }
 
-    // }).then(r=>(alert("Added New Class: " + className)))
+     fetch('http://localhost:8080/api/v1/addClass', { //idk where api is
+         method: "POST",
+         headers: {
+             "Content-Type":'application/json'
+         },
+         body: JSON.stringify(data),
+
+     }).then(r=>(alert("Added New Class: " + className)))
 
 })
