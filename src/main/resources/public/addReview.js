9edb3a2c0aa6ec7b6a1dc3f19d4a26d2729d9ca6
Esther Mathunny <emathunn@depaul.edu>
2022-10-21 12:00:28 -0500

    added AdditionalClassInfo.html & .js so he can work on rendering info for the class + reviews

--- src/main/resources/public/addReview.js
+++ src/main/resources/public/addReview.js
@@ -10,34 +10,16 @@ function updateDislikes(){
     likes = false;
 }
 
-
-//function onClick(){
-//    const email = document.getElementById('email');
-//    const course = document.getElementById('course').split(" ");
-//    let department = course[0];
-//        let number = course[1];
-//    const gpa = document.getElementById('gpa');
-//    const difficultyRating = document.getElementById('difficultyRating');
-//        likeDislikeNone = 0;
-//            if(likes){
-//                likeDislikeNone = 2;
-//            }else if(dislikes){
-//                likeDislikeNone = 1;
-//            }
-//                const classDescription = document.getElementById('classDescription');
-//alert(email +" "+course +" "+department +" "+number +" "+gpa +" "+difficultyRating +" YY"+likeDislikeNone +"YY "+classDescription);
-//
-//}
-
-
 const form = document.getElementById('formElem');
-const course = form.getElementById('course').split(" ");
+//const course = form.getElementById('course').split(" ");
 form.addEventListener('submit',function (e){
     e.preventDefault();
     const prepayload = new FormData(form)
     const payload = new URLSearchParams(prepayload);
 
     let email = payload.get("email");
+    let course = payload.get("course").split(" ");
+
     let department = course[0];
     let number = course[1];
     let gpa = payload.get("gpa");
@@ -45,13 +27,15 @@ form.addEventListener('submit',function (e){
     likeDislikeNone = 0;
     alert("hello");
     if(likes){
-        like = 2;
+        likeDislikeNone = 2;
     }else if(dislikes){
         likeDislikeNone = 1;
     }
     let classDescription = payload.get("classDescription");
-alert(likeDislikeNone);
-alert(email +" "+course +" "+department +" "+number +" "+gpa +" "+difficultyRating +" "+likeDislikeNone +" "+classDescription);
+    alert(likeDislikeNone);
+    alert(email +" "+course +" "+department +" "+number +" "+gpa +" "+difficultyRating +" "+likeDislikeNone +" "+classDescription);
+
+
 //call the API
      let data = {
 
