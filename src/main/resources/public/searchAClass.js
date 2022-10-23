ac5625f336a9514ae968e374f97bfbc327baef5b
Esther Mathunny <emathunn@depaul.edu>
2022-10-23 13:36:00 -0500

    added some changes

--- src/main/resources/public/searchAClass.js
+++ src/main/resources/public/searchAClass.js
@@ -13,55 +13,56 @@ form.addEventListener('submit',function (e){
 })
 
 function fetchAndRenderDetails(department, classNumber){
-alert("sent in");
-}
-let data = {
-        department:department,
-        classNumber: classNumber
-    }
-
-    fetch('http://localhost:8080/api/v1/getClassByNameAndNumber', {
-        method: "POST",
-        headers: {
-            "Content-Type":'application/json'
-        },
-        body: JSON.stringify(data),
-
-    }).then(r=>{
-    alert("Retrieved class: " + department + " " + classNumber;
-    alert(department+classNumber);
-    for (let i = 0; i < r.data.length; i++) {
-            const id = document.createElement('h2');
-            const className =  document.createElement("h3");
-            const classDescription =  document.createElement("h3");
-            const department =  document.createElement("h4");
-            const classNumber =  document.createElement("h5");
-            const cumGPA =  document.createElement("h5");
-            const cumDif = document.createElement("h5");
-            const likes = document.createElement("h5");
-            const dislikes = document.createElement("h5");
-
-
-            id.innerText = "Num: " + r.data[i].id;
-            className.innerHTML = "Class Name <a href=\"AdditionalClassInfo.html\">" + r.data[i].className + "</a>";
-            classDescription.innerHTML = "Description " + r.data[i].classDescription;
-            department.innerHTML = "Department " + r.data[i].department;
-            classNumber.innerHTML = "Class Number " +r.data[i].classNumber;
-            cumGPA.innerHTML = "Cumulative GPA " + r.data[i].cumGPA;
-            cumDif.innerHTML = "Cumulative Difference " + r.data[i].cumDif;
-            likes.innerHTML = "Likes " + r.data[i].likes;
-            dislikes.innerHTML = "Dislikes " + r.data[i].dislikes;
-
-            theUserSection.append(id);
-            theUserSection.append(className);
-            theUserSection.append(classDescription);
-            theUserSection.append(department);
-            theUserSection.append(classNumber);
-            theUserSection.append(cumGPA);
-            theUserSection.append(cumDif);
-            theUserSection.append(likes);
-            theUserSection.append(dislikes);
-
-
+    alert("sent in");
+    let data = {
+            department:department,
+            classNumber: classNumber
         }
+
+        fetch('http://localhost:8080/api/v1/getClassByNameAndNumber', {
+            method: "POST",
+            headers: {
+                "Content-Type":'application/json'
+            },
+            body: JSON.stringify(data),
+
+        }).then(r=>{
+        alert("Retrieved class: " + department + " " + classNumber;
+        alert(department+classNumber);
+        for (let i = 0; i < r.data.length; i++) {
+                const id = document.createElement('h2');
+                const className =  document.createElement("h3");
+                const classDescription =  document.createElement("h3");
+                const department =  document.createElement("h4");
+                const classNumber =  document.createElement("h5");
+                const cumGPA =  document.createElement("h5");
+                const cumDif = document.createElement("h5");
+                const likes = document.createElement("h5");
+                const dislikes = document.createElement("h5");
+
+
+                id.innerText = "Num: " + r.data[i].id;
+                className.innerHTML = "Class Name <a href=\"AdditionalClassInfo.html\">" + r.data[i].className + "</a>";
+                classDescription.innerHTML = "Description " + r.data[i].classDescription;
+                department.innerHTML = "Department " + r.data[i].department;
+                classNumber.innerHTML = "Class Number " +r.data[i].classNumber;
+                cumGPA.innerHTML = "Cumulative GPA " + r.data[i].cumGPA;
+                cumDif.innerHTML = "Cumulative Difference " + r.data[i].cumDif;
+                likes.innerHTML = "Likes " + r.data[i].likes;
+                dislikes.innerHTML = "Dislikes " + r.data[i].dislikes;
+
+                theUserSection.append(id);
+                theUserSection.append(className);
+                theUserSection.append(classDescription);
+                theUserSection.append(department);
+                theUserSection.append(classNumber);
+                theUserSection.append(cumGPA);
+                theUserSection.append(cumDif);
+                theUserSection.append(likes);
+                theUserSection.append(dislikes);
+
+
+            }
+}
+
 }
