6d51a0f344f945a3e3af222f84675b3b195b6676
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 <lpenrose@depaul.edu>
2022-10-25 13:48:26 -0500

    updated admin add class delete class

---
+++ src/main/resources/public/AdminDeleteClass.js
@@ -0,0 +1,29 @@
+const form = document.getElementById('formElem');
+
+form.addEventListener('submit',function (e){
+    e.preventDefault();
+    const prepayload = new FormData(form)
+
+    const payload = new URLSearchParams(prepayload);
+
+    let dpt = payload.get("department");
+    let cnm = payload.get("classNumber");
+
+    console.log(dpt)
+    console.log(cnm)
+
+    let data = {
+        department:dpt,
+        classNumber:cnm
+    }
+
+    fetch(`http://localhost:8080/api/v1/searchandremoveclass?department=${dpt}&classNumber=${cnm}`, {
+        method: 'DELETE',
+        headers: {
+            "Content-Type":'application/json'
+        },
+        body: JSON.stringify(data),
+
+    }).then(r=>(alert(dpt + " " + cnm + " Was Deleted")))
+
+})
