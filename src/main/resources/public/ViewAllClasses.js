7a47eb6a95d0ddf2fc79579165959b4718053ac9
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 <lpenrose@depaul.edu>
2022-10-27 04:46:05 -0500

    added bootstrap, cleaned up formating and typos

--- src/main/resources/public/ViewAllClasses.js
+++ src/main/resources/public/ViewAllClasses.js
@@ -6,14 +6,14 @@ let theUserTable = document.getElementById("tableBody");
     for (let i = 0; i < res.data.length; i++) {
 
         const id = document.createElement('h2');
-        const className =  document.createElement("h3");
-        const classDescription =  document.createElement("h3");
-        const department =  document.createElement("h4");
-        const classNumber =  document.createElement("h5");
-        const cumGPA =  document.createElement("h5");
-        const cumDif = document.createElement("h5");  
-        const likes = document.createElement("h5");  
-        const dislikes = document.createElement("h5");
+        const className =  document.createElement("h2");
+        const classDescription =  document.createElement("h4");
+        const department =  document.createElement("h6");
+        const classNumber =  document.createElement("h6");
+        const cumGPA =  document.createElement("h6");
+        const cumDif = document.createElement("h6");
+        const likes = document.createElement("h6");
+        const dislikes = document.createElement("h6");
 
         const addToPlannerBtn = document.createElement("button");
         addToPlannerBtn.className = "btn btn-info";
@@ -23,14 +23,14 @@ let theUserTable = document.getElementById("tableBody");
         // id.innerText = "Num: " + res.data[i].id;
         className.innerHTML = `<a href=\"AdditionalClassInfo.html?userReviewDepartment=${res.data[i].department}&userReviewCourseNumber=${res.data[i].classNumber}\">` + res.data[i].className + "</a>";
         className.className = "theClassName"
-        classDescription.innerHTML = "Description " + res.data[i].classDescription;
-        department.innerHTML = "Department " + res.data[i].department;
-        classNumber.innerHTML = "Class Number " +res.data[i].classNumber;
-        cumGPA.innerHTML = "Cumulitive GPA " + res.data[i].cumGPA; 
-        cumDif.innerHTML = "Cumulitive Difference " + res.data[i].cumDif; 
-        likes.innerHTML = "Likes " + res.data[i].likes; 
-        dislikes.innerHTML = "Dislikes " + res.data[i].dislikes;
-        addToPlannerBtn.innerText = "Add Course To Planner"
+        classDescription.innerHTML = res.data[i].classDescription;
+        department.innerHTML = "<br>Department: " + res.data[i].department;
+        classNumber.innerHTML = "Course Number: " +res.data[i].classNumber;
+        cumGPA.innerHTML = "Average GPA: " + res.data[i].cumGPA;
+        // cumDif.innerHTML = "Cumulitive Difference " + res.data[i].cumDif;
+        likes.innerHTML = "Total Likes: " + res.data[i].likes;
+        dislikes.innerHTML = "Total Dislikes: " + res.data[i].dislikes;
+        addToPlannerBtn.innerText = "Add To Planner"
 
 
         theUserSection.append(id);
