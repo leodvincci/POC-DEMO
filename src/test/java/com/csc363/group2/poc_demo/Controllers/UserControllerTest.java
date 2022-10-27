bec16edf7d457b1e3accbbeaabede113d4a357b9
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 <lpenrose@depaul.edu>
2022-10-26 21:00:33 -0500

    added JUnit test for whoAmIAPI, test passed

--- src/test/java/com/csc363/group2/poc_demo/Controllers/UserControllerTest.java
+++ src/test/java/com/csc363/group2/poc_demo/Controllers/UserControllerTest.java
@@ -1,7 +1,23 @@
 package com.csc363.group2.poc_demo.Controllers;
 
+import com.csc363.group2.poc_demo.Repos.UserRepository;
+import com.csc363.group2.poc_demo.Services.UserService;
+import com.csc363.group2.poc_demo.appuser.AppUser;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserControllerTest {
+
+    @Test
+    void whoAmItest(){
+        AppUser appUser = new AppUser();
+        appUser.setEmail("leodpenrose@gmail.com");
+        assertEquals("leodpenrose@gmail.com",appUser.getEmail());
+    }
 
 }
