a80af52b2f4376aeae52e968077960b6e505e3bb
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 <lpenrose@depaul.edu>
2022-11-05 14:45:11 -0500

    updated jUnit Test ClassEntity

--- src/test/java/com/csc363/group2/poc_demo/ClassEntity/ClassEntityTest.java
+++ src/test/java/com/csc363/group2/poc_demo/ClassEntity/ClassEntityTest.java
@@ -24,62 +24,88 @@ class ClassEntityTest {
 
     @Test
     void setDepartment() {
-
+        classEntity.setDepartment("College");
+        assertEquals("College",classEntity.getDepartment());
     }
 
     @Test
     void getClassName() {
+        assertEquals("Intro To Coding",classEntity.getClassName());
     }
 
     @Test
     void setClassName() {
+        classEntity.setClassName("Intro to Programming");
+        assertEquals("Intro to Programming",classEntity.getClassName());
     }
 
     @Test
     void getClassDescription() {
+        assertEquals("A Good Class",classEntity.getClassDescription());
     }
 
     @Test
     void setClassDescription() {
+        classEntity.setClassDescription("A very good class");
+        assertEquals("A very good class",classEntity.getClassDescription());
     }
 
     @Test
     void getClassNumber() {
+        assertEquals(101,classEntity.getClassNumber());
     }
 
     @Test
     void setClassNumber() {
+        classEntity.setClassNumber(202);
+        assertEquals(202, classEntity.getClassNumber());
     }
 
     @Test
     void getCumGPA() {
+        assertEquals(4.0, classEntity.getCumGPA());
     }
 
     @Test
     void setCumGPA() {
+        classEntity.setCumGPA(5.0);
+        assertEquals(5.0, classEntity.getCumGPA());
+
     }
 
     @Test
     void getCumDif() {
+        assertEquals(5, classEntity.getCumDif());
+
     }
 
     @Test
     void setCumDif() {
+        classEntity.setCumDif(45);
+        assertEquals(45, classEntity.getCumDif());
+
     }
 
     @Test
     void getLikes() {
+        assertEquals(10, classEntity.getLikes());
+
     }
 
     @Test
     void setLikes() {
+        classEntity.setLikes(200);
+        assertEquals(200, classEntity.getLikes());
     }
 
     @Test
     void getDislikes() {
+        assertEquals(2, classEntity.getDislikes());
     }
 
     @Test
     void setDislikes() {
+        classEntity.setDislikes(42);
+        assertEquals(42, classEntity.getDislikes());
     }
 }
