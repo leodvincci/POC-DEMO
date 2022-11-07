41af4e7759e7f424e9f73c3960a887d6d2bc8e66
Esther Mathunny <emathunn@depaul.edu>
2022-11-07 15:53:45 -0600

    added ui testing checks for AddClass.html

--- src/main/resources/public/addNewClass.js
+++ src/main/resources/public/addNewClass.js
@@ -1,32 +1,54 @@
 
 const form = document.getElementById('formElem');
-
-form.addEventListener('submit',function (e){
+function containsNumbers(str) {
+    return /\d/.test(str);
+}
+function containsOnlyNumbers(str) {
+    return /^\d+$/.test(str);
+}
+form.addEventListener('submit', function (e) {
     e.preventDefault();
+    alert("hello!");
     const prepayload = new FormData(form)
 
     const payload = new URLSearchParams(prepayload);
 
     let classDescription = payload.get("classDescription");
     let className = payload.get("className");
-    let department = payload.get("department");
-    let classNumber = payload.get("classNumber")
+    let dpt = payload.get("department");
+    let cnm = payload.get("classNumber")
+    if(containsNumbers(className)){
+        alert("Your class name contains numbers. Please stick to Roman Numerals!");
+    }
+    else if (!((dpt.length >= 1) && (dpt.length <= 4))) {
+        alert("Invalid Department!");
+    } else if (containsNumbers(dpt)) {
+        alert("Invalid Department!");
+    } else if (cnm.length > 5) {
+        alert("Invalid Course Number!");
+    }
+    else if (!containsOnlyNumbers(cnm)) {
+        alert("Invalid Course Number!");
+    } else if ((dpt.length == 0) || (cnm.length == 0) || (className.length == 0)|| (classDescription.length == 0)) {
+        alert("Please fill in all fields.");
+    } else {
+        let data = {
+            classDescription: classDescription,
+            className: className,
+            department: dpt,
+            classNumber: cnm
+        }
 
+        fetch('http://localhost:8080/api/v1/addClass', {
+            method: "POST",
+            headers: {
+                "Content-Type": 'application/json'
+            },
+            body: JSON.stringify(data),
 
-    let data = {
-        classDescription:classDescription,
-        className:className,
-        department:department,
-        classNumber: classNumber
+        }).then(r => (alert("Added New Class: " + className)))
     }
 
-    fetch('http://localhost:8080/api/v1/addClass', {
-        method: "POST",
-        headers: {
-            "Content-Type":'application/json'
-        },
-        body: JSON.stringify(data),
 
-    }).then(r=>(alert("Added New Class: " + className)))
 
 })
