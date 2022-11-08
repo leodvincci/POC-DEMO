9e702a50349a43946752590d6d76667836608447
Esther Mathunny <emathunn@depaul.edu>
2022-11-07 21:54:43 -0600

    added input checks for AddReview.html

--- src/main/resources/public/addReview.js
+++ src/main/resources/public/addReview.js
@@ -1,60 +1,110 @@
 var likes = false;
 var dislikes = false;
 
-function updateLikes(){
+function updateLikes() {
     likes = true;
     dislikes = false;
 }
-function updateDislikes(){
+function updateDislikes() {
     dislikes = true;
     likes = false;
 }
 
 const form = document.getElementById('formElem');
-//const course = form.getElementById('course').split(" ");
-form.addEventListener('submit',function (e){
+function isFloat(value) {
+    if (
+      typeof value === 'number' &&
+      !Number.isNaN(value) &&
+      !Number.isInteger(value)
+    ) {
+      return true;
+    }
+  
+    return false;
+  }
+function containsNumbers(str) {
+    return /\d/.test(str);
+  }
+  function containsOnlyNumbers(str) {
+    return /^\d+$/.test(str);
+  }
+form.addEventListener('submit', function (e) {
     e.preventDefault();
+
     const prepayload = new FormData(form)
     const payload = new URLSearchParams(prepayload);
 
     let email = payload.get("email");
     let course = payload.get("course").split(" ");
     console.log(course)
-    let department = course[0];
-    let number = course[1];
+    let dpt = course[0];
+    let cnm = course[1];
     let gpa = payload.get("gpa");
     let difficultyRating = payload.get("difficultyRating");
     likeDislikeNone = 0;
-    // alert("hello");
-    if(likes){
+    
+    if (likes) {
         likeDislikeNone = 2;
-    }else if(dislikes){
+    } else if (dislikes) {
         likeDislikeNone = 1;
     }
     let classDescription = payload.get("classDescription");
-    alert("Thanks For Leaving A Rating!");
-    // alert(email +" "+course +" "+department +" "+number +" "+gpa +" "+difficultyRating +" "+likeDislikeNone +" "+classDescription);
+    var ampersandFound = false;
+    var dotFound = false;
 
+    
+    var i = 0;
+    for(i; i<email.length; i++){
+        if(email[i] == '@' ){
+            ampersandFound = true;
+        }
+        else if(email[i] == '.'){
+            dotFound = true;
+        }
+    }
+    if ((dpt.length == 0) || (cnm.length == 0)|| (gpa.length == 0)|| (email.length == 0)|| (difficultyRating.length == 0)|| (classDescription.length == 0)) {
+        alert("Please fill in all fields.");
+    }
+    else if(!(ampersandFound && dotFound)){
+        alert('Invalid Email!');
+    }else if (!((dpt.length >= 1) && (dpt.length <= 4))) {
+        alert("Invalid Department!");
+    } else if (containsNumbers(dpt)) {
+        alert("Invalid Department!");
+    } else if (cnm.length > 5) {
+        alert("Invalid Course Number!");
+    }else if (!containsOnlyNumbers(cnm)) {
+        alert("Invalid Course Number!");
+    } 
+    // else if(isFloat(gpa)){
+    //     alert("Invalid GPA!");
+    // } 
+    else if(!((difficultyRating >= 1) && (difficultyRating <= 10))){
+        alert("Invalid Difficulty Rating!");
+    }
+    else {
+        alert("Thanks For Leaving A Rating!");
+        //call the API
+        let data = {
+            userReviewEmail: email,
+            userReviewDepartment: course[0],
+            userReviewCourseNumber: course[1],
+            userReviewGPA: gpa,
+            userReviewDifficulty: difficultyRating,
+            userReviewLike: likes,
+            userReviewDislike: dislikes,
+            userReviewFeedback: classDescription
+        }
 
-//call the API
-    let data = {
-        userReviewEmail:email,
-        userReviewDepartment:course[0],
-        userReviewCourseNumber: course[1],
-        userReviewGPA:gpa,
-        userReviewDifficulty:difficultyRating,
-        userReviewLike:likes,
-        userReviewDislike: dislikes,
-        userReviewFeedback:classDescription
+        fetch('http://54.146.226.153:8080/api/v1/addUserReview', { //idk where api is
+            method: "POST",
+            headers: {
+                "Content-Type": 'application/json'
+            },
+            body: JSON.stringify(data),
+
+        }).then(r => (alert("Added New Review: " + className)))
     }
 
-     fetch('http://54.146.226.153:8080/api/v1/addUserReview', { //idk where api is
-         method: "POST",
-         headers: {
-             "Content-Type":'application/json'
-         },
-         body: JSON.stringify(data),
-
-     }).then(r=>(alert("Added New Review: " + className)))
 
 })
