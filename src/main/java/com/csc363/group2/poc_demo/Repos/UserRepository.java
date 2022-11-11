3a0c58aa9b11ababb6dd9050a93ad21493cc57eb
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 <lpenrose@depaul.edu>
2022-11-11 14:16:35 -0600

    created api for getSavedClasses

--- src/main/java/com/csc363/group2/poc_demo/Repos/UserRepository.java
+++ src/main/java/com/csc363/group2/poc_demo/Repos/UserRepository.java
@@ -1,9 +1,18 @@
 package com.csc363.group2.poc_demo.Repos;
 
+import com.csc363.group2.poc_demo.ClassEntity.ClassEntity;
+import com.csc363.group2.poc_demo.UserReview.UserReviewEntity;
 import com.csc363.group2.poc_demo.appuser.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<AppUser,Long> {
+
+    AppUser getAppUserByEmail(String userEmail);
+
+
+
 }
