8ef382f1e28c873a551a081f8ba3cce5c57cca1b
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 <lpenrose@depaul.edu>
2022-11-14 13:09:55 -0600

    created deletesavedcourse api on backend to delete a saved course and implemented feature on the front end

--- src/main/java/com/csc363/group2/poc_demo/ClassRepo/ClassRepository.java
+++ src/main/java/com/csc363/group2/poc_demo/ClassRepo/ClassRepository.java
@@ -4,7 +4,10 @@ import com.csc363.group2.poc_demo.ClassEntity.ClassEntity;
 import com.csc363.group2.poc_demo.appuser.AppUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,5 +19,12 @@ public interface ClassRepository extends JpaRepository<ClassEntity,Long> {
     ClassEntity findByDepartmentAndClassNumber(String department, int classNumber);
 
     List<ClassEntity> findAllByUsers(AppUser user);
+
+    @Transactional
+    @Modifying
+    @Query(value = "delete from student_course_map where course_id= course_id and appuser_id= appuser_id ",nativeQuery = true)
+    void deleteByClassNumberAndUserId(int course_id, Long appuser_id);
+
+
 
 }
